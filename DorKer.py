c13eaaea9300d00bc9082277866fb9c6ebaa565a
Sandman <francisco.vera.95@gmail.com>
2016-12-10 18:22:52 -0600

    Update DorKer.py

--- DorKer.py
+++ DorKer.py
@@ -7,55 +7,148 @@
 # Version         : 1.0
 #  
 
-
+import re 
+import urllib2
+import sqlscann
+import commands
 import os
+import csv
 import sys
 import subprocess
-import commands
-import urllib2
-import urllib
-import mechanize
-import re
-import random
-
-
+from colorama import *
 
 def logo():
-	print("Piensa en un bonito logo :3")
+	print Fore.RED+"________                ____  __.            "
+	print "\______ \   ___________|    |/ _|___________ "
+	print " |    |  \ /  _ \_  __ \      <_/ __ \_  __ \ "
+	print " |    `   (  <_> )  | \/    |  \  ___/|  | \/"
+	print "/_______  /\____/|__|  |____|__ \___  >__|   "
+	print "        \/                     \/   \/       "
+	print "              By: Sandman"
+	print "Correo y Xmpp: sandman95@cock.li twitter: @Sandman__95"
+
+def clean():
+
+	if sys.platform == "linux" or sys.platform == "linux2":
+		subprocess.call(["clear"], shell=True)
+
+	else:
+		subprocess.call(["cls"], shell=True)
+		
+clean()			
+logo()
+print """
+    [1] Ingresar Dork
+    [2] Utilizar txt con diferentes dorks
+"""
 	
-def finder():
-	logo()
-	useragent = ['Mozilla/4.0 (compatible; MSIE 5.0; SunOS 5.10 sun4u; X11)',
-            'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.2pre) Gecko/20100207 Ubuntu/9.04 (jaunty) Namoroka/3.6.2pre',
-            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser;',
-            'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)',
-            'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1)',
-            'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6)',
-            'Microsoft Internet Explorer/4.0b1 (Windows 95)',
-            'Opera/8.00 (Windows NT 5.1; U; en)',
-            'Mozilla/4.0 (compatible; MSIE 5.0; AOL 4.0; Windows 95; c_athome)',
-            'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
-            'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
-            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; ZoomSpider.net bot; .NET CLR 1.1.4322)',
-            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; QihooBot 1.0 qihoobot@qihoo.net)',
-            'Mozilla/4.0 (compatible; MSIE 5.0; Windows ME) Opera 5.11 [en]']
-	headers = random.choice(useragent)
-	header = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.6)'}
-	browser = mechanize.Browser()
-	browser.set_handle_robots(False)
-	browser.addheaders = [('User-agent',useragent)]
-	page = ["0"]
-	print('Ingresa tu Dork: ')
-	dork = str(raw_input('----->   '))
-	for i in page:
-		try:
-			duck =  "https://duckduckgo.com/q="+dork+"?ia=web"+i
-			html = browser.open(duck).read()
-			print (html)
-			patron = re.compile("<h3 class=\"r\"><a href=\"/url\?q=(.*?)&amp.*?\">.*?</a></h3>")
-			found = re.findall(patron,html)
-			print (found,"lol")
-		except:
-			pass
-finder()
+while True:
+	respuesta = raw_input("=>:  ")
+	if respuesta == "1":
+		print Fore.WHITE+"DorKer"
+		print ""
+		impr = raw_input("Te Gustaria guardar las URL's en un Documento? Y/n  ")
+		dork = raw_input("Dork: ")
+		if " " in dork:
+			dork=dork.replace(" ", "+")
+
+		def finder(dork):
+			first = 1
+			while first<=500:
+				response = urllib2.urlopen("http://www.bing.com/search?q={}&first={}".format(dork, first))
+				payload = response.read()
+				results = re.findall('<h2><a(.*?)h="', payload)
+				for result in results:
+					result = result.lstrip(" href=")
+					result = result.lstrip('"')
+					result = result.rstrip('" ')
+					vict = result
+					sqlscann.scan(vict)
+					first = first + 10
+		def printer(dork):
+			first = 1
+			while first<=500:
+				response = urllib2.urlopen("http://www.bing.com/search?q={}&first={}".format(dork, first))
+				payload = response.read()
+				results = re.findall('<h2><a(.*?)h="', payload)
+				for result in results:
+					result = result.lstrip(" href=")
+					result = result.lstrip('"')
+					result = result.rstrip('" ')
+					vict = result
+					sqlscann.iprim(vict)
+					first = first + 10
+		if impr == "y" or impr == "Y" or impr == "":
+			printer(dork)
+		elif impr == "N" or impr == "n": 
+			finder(dork)
+		print ""
+		print "Listo"
 	
+	elif respuesta == "2":
+		print Fore.WHITE+"Dorker"
+		print ""
+		impr = raw_input("Te Gustaria guardar las URL's en un Documento? Y/n  ")
+		print "Introduce la ruta de tus dorks"
+		print "ex. /home/user/dorks.txt"
+		
+		dork = raw_input("=>:  ")
+		lista = commands.getoutput("cat "+dork)
+		os.system("echo '"+lista+"' > Dorks.txt")
+		dorklist = open("Dorks.txt","r")
+		reader = csv.reader(dorklist)
+		lista = [row for row in reader]
+		nl = 0
+		dork = str(lista [nl])
+		i = 1
+		print len(lista)
+		while True:
+			
+			if "['" in dork:
+				dork=dork.replace("['", "")
+				dork=dork.replace("']","")
+			if " " in dork:
+				dork=dork.replace(" ", "+")
+
+			def finder(dork):
+				print (Fore.YELLOW+"Buscando el Dorck "+dork)
+				first = 1
+				while first<=500:
+					response = urllib2.urlopen("http://www.bing.com/search?q={}&first={}".format(dork, first))
+					payload = response.read()
+					results = re.findall('<h2><a(.*?)h="', payload)
+					for result in results:
+						result = result.lstrip(" href=")
+						result = result.lstrip('"')
+						result = result.rstrip('" ')
+						vict = result
+						sqlscann.scan(vict)
+						first = first + 10
+			def printer(dork):
+				print (Fore.YELLOW+"Buscando el Dorck "+dork)
+				first = 1
+				while first<=500:
+					response = urllib2.urlopen("http://www.bing.com/search?q={}&first={}".format(dork, first))
+					payload = response.read()
+					results = re.findall('<h2><a(.*?)h="', payload)
+					for result in results:
+						result = result.lstrip(" href=")
+						result = result.lstrip('"')
+						result = result.rstrip('" ')
+						vict = result
+						sqlscann.iprim(vict)
+						first = first + 10
+			if impr == "y" or impr == "Y" or impr == "":
+				printer(dork)
+			elif impr == "N" or impr == "n": 
+				finder(dork)
+			nl = nl + 1
+			i = i + 1
+			if i <= len(lista):
+				dork = str(lista [nl])
+			elif i > len(lista):
+				print ""
+				print "Todos Los Dorks Escaneados..."
+				os.system("rm Dorks.txt")
+				sys.exit(0)
+		
